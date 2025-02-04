d1574c506ef62af8b6de0679622e5790a7d4132f
Viktoriia Petriv <viktoriia.petriv.22@pnu.edu.ua>
2025-02-04 19:09:33 +0200

    Init project

---
+++ src/main/java/org/pnurecord/recordbook/RecordBookApplication.java
@@ -0,0 +1,13 @@
+package org.pnurecord.recordbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecordBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecordBookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/pnurecord/recordbook/RecordBookApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pnurecord.recordbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecordBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
