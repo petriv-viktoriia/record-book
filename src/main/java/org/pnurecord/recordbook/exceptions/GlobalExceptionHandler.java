91dd110689494c8b1ca039fb5821525781adfa3a
petriv-viktoriia <150630919+petriv-viktoriia@users.noreply.github.com>
2025-02-09 20:56:14 +0200

    Supporting files in db

--- src/main/java/org/pnurecord/recordbook/exceptions/GlobalExceptionHandler.java
+++ src/main/java/org/pnurecord/recordbook/exceptions/GlobalExceptionHandler.java
@@ -1,10 +1,12 @@
 package org.pnurecord.recordbook.exceptions;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
 
 import java.util.stream.Collectors;
 
@@ -44,6 +46,16 @@ public class GlobalExceptionHandler {
                 HttpStatus.CONFLICT.name(),
                 HttpStatus.CONFLICT.value(),
                 ex.getMessage()
+        );
+    }
+
+    @ResponseStatus(HttpStatus.PAYLOAD_TOO_LARGE)
+    @ExceptionHandler(MaxUploadSizeExceededException.class)
+    public ResponseDto handleMaxSizeException(MaxUploadSizeExceededException ex) {
+        return new ResponseDto(
+                HttpStatus.PAYLOAD_TOO_LARGE.name(),
+                HttpStatus.PAYLOAD_TOO_LARGE.value(),
+                "File size exceeds the maximum allowed limit!"
         );
     }
 }
